be240c1f9d3bc494561714fd65b78d5f8d4be007
hrishi_qode <hjadhav@qodeleaf.com>
2023-02-28 16:07:56 +0530

    Add migrations for intializing tables

---
+++ migrations/20230228092710-create-user.cjs
@@ -0,0 +1,41 @@
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('user', {
+      userId: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      firstname: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      lastname: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      dateOfBirth: {
+        type: Sequelize.DATEONLY,
+        allowNull: false,
+      },
+      password: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      username: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true,
+      },
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true,
+      },
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('user');
+  },
+};
