7e6ccd89c1eaaeec6d908adf6c3e4ad0ab3e5cdf
hrishi_qode <hjadhav@qodeleaf.com>
2023-03-02 20:22:01 +0530

    Add api for viewing attendance

--- controllers/index.js
+++ controllers/index.js
@@ -1,8 +1,10 @@
 import { Router } from 'express';
 import attendanceRouter from './attendance-controller.js';
 import loginRouter from './login-controller.js';
+import viewAttendanceRouter from './view-attendance-controller.js';
 
 const centralRouter = Router();
 centralRouter.use(loginRouter);
 centralRouter.use(attendanceRouter);
+centralRouter.use(viewAttendanceRouter);
 export default centralRouter;
