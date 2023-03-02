907da947ebb0ab2c9d42efa304c77890027c9db3
hrishi_qode <hjadhav@qodeleaf.com>
2023-03-02 14:44:14 +0530

    Add functionality for signin and signout in office

--- controllers/index.js
+++ controllers/index.js
@@ -1,7 +1,8 @@
 import { Router } from 'express';
+import attendanceRouter from './attendance-controller.js';
 import loginRouter from './login-controller.js';
 
 const centralRouter = Router();
-
 centralRouter.use(loginRouter);
+centralRouter.use(attendanceRouter);
 export default centralRouter;
