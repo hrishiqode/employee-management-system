efcd03d6879ef35d1234c0a4ad93d0e13235fbcb
M Fahim KHAN <mkhan@qodeleaf.com>
2023-03-06 16:36:30 +0530

    Add feature of table in view all user

--- controllers/hr-controllers/hr-view-user-list.js
+++ controllers/hr-controllers/hr-view-user-list.js
@@ -5,10 +5,17 @@ import db from '../../models/index.cjs';
 const hrViewUserListRouter = Router();
 hrViewUserListRouter.route('/users')
   .get(async (req, res, next) => {
-    const employee = await db.users.findAll({
+    const users = await db.users.findAll({
       where: {
         userId: req.session.user.id,
       },
+      attributes: ['id', 'firstName', 'lastName'],
+    });
+    res.render('hr-view-users', {
+      firstName: req.session.user.firstName,
+      lastname: req.session.user.lastName,
+      id: req.session.user.id,
+      userArray: users,
     });
   });
 export default hrViewUserListRouter;
