5fdba797f3b9ba3691e058b003737178057a028b
hrishi_qode <hjadhav@qodeleaf.com>
2023-03-06 11:34:47 +0530

    changed port to 8080

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import configure from './configurations/index.js';
 import centralRouter from './controllers/index.js';
 
 const mainServer = express();
-const port = 8081;
+const port = 8080;
 mainServer.listen(port);
 configure(mainServer);
 mainServer.use(centralRouter);
