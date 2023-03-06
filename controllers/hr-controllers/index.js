547e337ac08b28eaa869557db6f35dba966f1b2c
hrishi_qode <hjadhav@qodeleaf.com>
2023-03-06 12:41:27 +0530

    Add controller for viewing user list

--- controllers/hr-controllers/index.js
+++ controllers/hr-controllers/index.js
@@ -2,9 +2,11 @@ import { Router } from 'express';
 import LoginAuthentication from '../../services/login-authentication.js';
 import hrAddEmployeeRouter from './hr-add-employee-controller.js';
 import hrDashboardRouter from './hr-dashboard-controller.js';
+import hrViewUserListRouter from './hr-view-user-list.js';
 
 const hrRouter = Router();
 hrRouter.use(LoginAuthentication.hrCheck);
 hrRouter.use(hrDashboardRouter);
 hrRouter.use(hrAddEmployeeRouter);
+hrRouter.use(hrViewUserListRouter);
 export default hrRouter;
