0084e989a2c895c630e9f8bca4daebde5de5685b
M Fahim KHAN <mkhan@qodeleaf.com>
2023-03-06 16:36:30 +0530

    Add feature of table in view all user

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import configure from './configurations/index.js';
 import centralRouter from './controllers/index.js';
 
 const mainServer = express();
-const port = 8080;
+const port = 8081;
 mainServer.listen(port);
 configure(mainServer);
 mainServer.use(centralRouter);
