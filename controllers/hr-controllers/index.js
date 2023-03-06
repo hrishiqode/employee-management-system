f777fa2c5a71bdfbdd9af9679ba4af28ad43f6b8
hrishi_qode <hjadhav@qodeleaf.com>
2023-03-06 13:56:05 +0530

    Add contact api for hr and its template

--- controllers/hr-controllers/index.js
+++ controllers/hr-controllers/index.js
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 import LoginAuthentication from '../../services/login-authentication.js';
 import hrAddEmployeeRouter from './hr-add-employee-controller.js';
+import hrContactRouter from './hr-contact-controller.js';
 import hrDashboardRouter from './hr-dashboard-controller.js';
 
 const hrRouter = Router();
 hrRouter.use(LoginAuthentication.hrCheck);
 hrRouter.use(hrDashboardRouter);
 hrRouter.use(hrAddEmployeeRouter);
+hrRouter.use(hrContactRouter);
 export default hrRouter;
