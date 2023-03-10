fce4617964ac37f983b9d6dc59e71561afdf9b43
hrishi_qode <hjadhav@qodeleaf.com>
2023-03-10 16:03:16 +0530

    modified sequelize connection for checking env

--- models/index.cjs
+++ models/index.cjs
@@ -9,7 +9,7 @@ const env = process.env.NODE_ENV || 'development';
 const config = require(`${__dirname}/../config/config.json`)[env];
 const db = {};
 let sequelize;
-if (config.use_env_variable) {
+if (process.env.connString) {
   sequelize = new Sequelize(process.env.connString);
 } else {
   sequelize = new Sequelize(config.database, config.username, process.env.databasePassword, config);
